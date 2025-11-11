75b632472785d0f61d5ba1ea46ff203f541596e7
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-11 12:56:34 +0100

    Aggiungo la lista dei film  nel componente App. aggiungo useEffect per stampare in console la lista dei film

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+  const [movieSelection, setMovieSelection] = useState(movies);
+
+  useEffect(() => {
+    console.log('Lista dei film:', movieSelection);
+  }, []);
 
   return (
     <>
-
+      <h1>Lista Film</h1>
+      <ul>
+        {movieSelection.map((movie, index) => (
+          <li key={index}>{movie.title} - {movie.genre}</li>
+        ))}
+      </ul>
     </>
   )
 }
